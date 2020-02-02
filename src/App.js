5e74ccae9eea956abc7b81b0e52e018b566c6f93
Julio Martins de Sousa <jmasousa@voegol.com.br>
2020-02-02 20:45:17 -0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-	return <div className="App" />;
+	const [techs, setTech] = useState([
+		'Android - Java / Kotlin',
+		'iOS - Obj C / Swift',
+	]);
+	const [newTech, setNewTech] = useState('');
+
+	function handleAdd() {
+		if (newTech.length > 0) {
+			setTech([...techs, newTech]);
+		}
+	}
+
+	function handleNewTech(e) {
+		setNewTech(e.target.value);
+	}
+
+	return (
+		<>
+			<p>Hooks - useState</p>
+			<ul>
+				{techs.map(tech => (
+					<li key={tech}>{tech}</li>
+				))}
+			</ul>
+			<input value={newTech} onChange={handleNewTech} />
+			<button type="button" onClick={handleAdd}>
+				Adicionar
+			</button>
+		</>
+	);
 }
 
 export default App;
